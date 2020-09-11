8765d7fa994b17908c3b68058fb4c0cbbf04bb35
Stacy Daniel <stacydn23@gmail.com>
2020-09-11 01:06:08 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,15 +1,24 @@
 function basicTeenager(age) {
-  if (age >13 && age <=19) {return " you are a teenger"}
+  if (age >13 && age <=19)
+  return " you are a teenger!"
 
 }
 
 function teenager(age) {
   if (age >=13 && age <=19)
+  return "you are a teenage!"
+   {else}
+   return " you are a teenger!"
 
 }
 
 function ageChecker(age) {
   if (age >=13 && age <=19)
+  return "you are a teenger!"
+  else if (age<=12)
+  return  "you are a kid"
+  else if (age >19)
+  return "you are a grownup"
 
 }
 
